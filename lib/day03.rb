2ff7555c391ae14d547c0dd360dc51f6356120a9
Eliza Brock Marcum <eliza@elizamarcum.com>
2025-11-08 09:07:11 -0800

    Day 3, Part 2

--- lib/day03.rb
+++ lib/day03.rb
@@ -1,17 +1,33 @@
 class Day3
   def self.parse_input(input)
-    output = []
-    input.scan(/mul\((\d+),(\d+)\)/) do |left, right|
-      output << [left.to_i, right.to_i]
+    commands = []
+    input.scan(/(don't)\(\)|(do)\(\)|(mul)\((\d+),(\d+)\)/) do |match|
+      command, left, right = match.compact
+      commands << {name: command, args: [left.to_i, right.to_i]}
     end
-    output
+    commands
   end
 
   def self.part1(input)
-    parse_input(input).map{ |pair| pair[0] * pair[1] }.sum
+    parse_input(input)
+      .filter{|command| command[:name] == 'mul'}
+      .map{ |command| command[:args][0] * command[:args][1] }.sum
   end
 
   def self.part2(input)
-    "TBD"
+    enabled = true
+    parse_input(input).reduce(0) do |sum, command|
+      case command[:name]
+      when "do"
+        enabled = true
+      when "don't"
+        enabled = false
+      when "mul"
+        if enabled
+          sum += command[:args][0] * command[:args][1]
+        end
+      end
+      sum
+    end
   end
 end
--- spec/day03_spec.rb
+++ spec/day03_spec.rb
@@ -5,7 +5,7 @@ require_relative 'spec_helper'
 describe Day3 do
   let(:input) {
     <<~STRING
-      xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+      xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
     STRING
   }
 
@@ -19,7 +19,7 @@ describe Day3 do
 
   describe "part2" do
     it "overall scenario" do
-      expected = "TBD"
+      expected = 48
       actual = Day3.part2(input)
       _(actual).must_equal(expected)
     end
