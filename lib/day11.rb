25c3ac94c0c29825c650fe9a05869ff9d860eb9b
Eliza Brock Marcum <eliza@elizamarcum.com>
2025-11-21 11:14:47 -0800

    Day 11, Part 2: Caching edition

--- lib/day11.rb
+++ lib/day11.rb
@@ -1,75 +1,86 @@
 class Day11
   def self.part1(input)
-    blink!(input, 25)
+    stones = PhysicsDefyingStones.new(input)
+    stones.count_after_rapid_blinks(25)
   end
 
   def self.part2(input)
-    blink!(input, 75)
-  end
-
-  def self.blink!(input, n)
     stones = PhysicsDefyingStones.new(input)
-    n.times do |i|
-      puts "Blink #{i+1} of #{n}"
-      stones.blink!
-    end
-    stones.count
+    stones.count_after_rapid_blinks(75)
   end
 
   class PhysicsDefyingStones
+    @@cache = {}
     attr_reader :stones
 
     def initialize(input)
       @stones = input.split(" ").map(&:to_i)
+      @precalculated_results = {}
     end
 
-    def blink!
-      new_stones = Array.new(@stones.length * 1.5)
-      index = 0
-      @stones.each do |stone|
-        break if stone.nil?
-        if stone == 0
-          # If the stone is engraved with the number 0, it is replaced by a stone engraved
-          # with the number 1.
-          new_stones[index] =  1
-          index += 1
-        else
-          digits = stone.to_s
-          if digits.length.even?
-            # If the stone is engraved with a number that has an even number of digits, it is
-            # replaced by two stones. The left half of the digits are engraved on the new left
-            # stone, and the right half of the digits are engraved on the new right stone. (The
-            # new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-            new_stones[index] = digits.slice!(0...digits.length/2).to_i
-            new_stones[index+1] =  digits.to_i
-            index += 2
-          else
-            # If none of the other rules apply, the stone is replaced by a new stone; the old
-            # stone's number multiplied by 2024 is engraved on the new stone.
-            new_stones[index] = stone * 2024
-            index += 1
-          end
-        end
-      end
-
-      @stones = new_stones
+    def count_after_rapid_blinks(n=1)
+      @stones.sum{ |stone| self.class.descendant_count(for_stone: stone, at_generation: n) }
     end
 
     def count
       @stones.count
     end
 
-    def inspect
-      "#<PhysicsDefyingStones::#{object_id} #{to_s}>"
+    private
+
+    def self.calculate_descendant_counts(stone, generations)
+      return unless generations > 0
+
+      next_generation = immediate_descendants_for(stone)
+      if generations > 1
+        results = coalesce_descendant_counts_for(next_generation, generations - 1)
+      else
+        results = []
+      end
+      results.unshift next_generation.count
+
+      results
     end
 
-    def to_s
-      @stones.reject(&:nil?).map(&:to_s).join(" ")
+    def self.coalesce_descendant_counts_for(list, generations)
+      lists_of_descendant_counts = list.map{ |i| descendant_counts_for(i, through_at_least: generations) }
+      results = []
+      (Array.new(generations, 0)).zip(*lists_of_descendant_counts){ |sub_array| results << sub_array.sum }
+      results
     end
 
-    def ==(other)
-      return false unless other.is_a? PhysicsDefyingStones
-      @stones.compact == other.stones.compact
+    def self.descendant_count(for_stone: nil, at_generation: nil)
+      cache_hit = descendant_counts_for(for_stone, through_at_least: at_generation)
+      cache_hit[at_generation - 1]
+    end
+
+    def self.descendant_counts_for(stone, through_at_least: 1, fill_in_cache_misses: true)
+      unless @@cache.has_key?(stone) && @@cache[stone].length >= through_at_least
+        if fill_in_cache_misses
+          @@cache[stone] = PhysicsDefyingStones::calculate_descendant_counts(stone, through_at_least)
+        end
+      end
+
+      @@cache[stone]
+    end
+
+    def self.immediate_descendants_for(i)
+      # If the stone is engraved with the number 0, it is replaced by a stone engraved
+      # with the number 1.
+      return [ 1 ] if i == 0
+
+      digits = i.to_s
+      if digits.length.even?
+        # If the stone is engraved with a number that has an even number of digits, it is
+        # replaced by two stones. The left half of the digits are engraved on the new left
+        # stone, and the right half of the digits are engraved on the new right stone. (The
+        # new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+        [ digits.slice!(0...digits.length/2).to_i, digits.to_i ]
+      else
+        # If none of the other rules apply, the stone is replaced by a new stone; the old
+        # stone's number multiplied by 2024 is engraved on the new stone.
+        [ i * 2024 ]
+      end
     end
   end
 end
--- spec/day11_spec.rb
+++ spec/day11_spec.rb
@@ -11,8 +11,16 @@ describe Day11 do
     end
   end
 
+  describe "part2" do
+    it "overall scenario" do
+      expected = 65601038650482
+      actual = Day11.part2('125 17')
+      _(actual).must_equal(expected)
+    end
+  end
+
   describe Day11::PhysicsDefyingStones do
-    describe "#blink!" do
+    describe "#count_after_rapid_blinks" do
       let(:state0){ Day11::PhysicsDefyingStones.new("125 17") }
       let(:state1){ Day11::PhysicsDefyingStones.new("253000 1 7") }
       let(:state2){ Day11::PhysicsDefyingStones.new("253 0 2024 14168") }
@@ -20,41 +28,77 @@ describe Day11 do
       let(:state4){ Day11::PhysicsDefyingStones.new("512 72 2024 2 0 2 4 2867 6032") }
       let(:state5){ Day11::PhysicsDefyingStones.new("1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32") }
       let(:state6){ Day11::PhysicsDefyingStones.new("2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2") }
-
       it 'transitions from state 0 to state 1' do
-        state0.blink!
-        _(state0).must_equal(state1)
+        actual = state0.count_after_rapid_blinks
+        _(actual).must_equal(state1.count)
       end
 
       it 'transitions from state 1 to state 2' do
-        state1.blink!
-        _(state1).must_equal(state2)
+        actual = state1.count_after_rapid_blinks
+        _(actual).must_equal(state2.count)
       end
 
       it 'transitions from state 2 to state 3' do
-        state2.blink!
-        _(state2).must_equal(state3)
+        actual = state2.count_after_rapid_blinks
+        _(actual).must_equal(state3.count)
       end
 
       it 'transitions from state 3 to state 4' do
-        state3.blink!
-        _(state3).must_equal(state4)
+        actual = state3.count_after_rapid_blinks
+        _(actual).must_equal(state4.count)
       end
 
       it 'transitions from state 4 to state 5' do
-        state4.blink!
-        _(state4).must_equal(state5)
+        actual = state4.count_after_rapid_blinks
+        _(actual).must_equal(state5.count)
       end
 
       it 'transitions from state 5 to state 6' do
-        state5.blink!
-        _(state5).must_equal(state6)
+        actual = state5.count_after_rapid_blinks
+        _(actual).must_equal(state6.count)
       end
 
       it 'transitions from initial state all the way through' do
         stones = Day11::PhysicsDefyingStones.new("125 17")
-        6.times { stones.blink! }
-        _(stones).must_equal(state6)
+        actual = stones.count_after_rapid_blinks(6)
+        _(actual).must_equal(state6.count)
+      end
+
+      describe "caching precalculated results" do
+        def get_cached_result_for(stone, generations)
+          Day11::PhysicsDefyingStones.descendant_counts_for(stone, through_at_least: generations, fill_in_cache_misses: false)[generations-1]
+        end
+
+        before do
+          stones = Day11::PhysicsDefyingStones.new("125 17 17")
+          # 17 -> 1 7 -> 2024 14168 -> 20 24 28676032 -> 2 0 2 4 2867 6032
+          #   -> 2 -> 2 -> 3 -> 6
+          stones.count_after_rapid_blinks(4)
+        end
+
+        it 'caches interim (1) results cached from #count_after_rapid_blinks' do
+          expected = 2
+          actual = get_cached_result_for(17, 1)
+          _(actual).must_equal(expected)
+        end
+
+        it 'caches interim (2) results cached from #count_after_rapid_blinks' do
+          expected = 2
+          actual = get_cached_result_for(17, 2)
+          _(actual).must_equal(expected)
+        end
+
+        it 'caches interim (3) results cached from #count_after_rapid_blinks' do
+          expected = 3
+          actual = get_cached_result_for(17, 3)
+          _(actual).must_equal(expected)
+        end
+
+        it 'caches final results cached from #count_after_rapid_blinks' do
+          expected = 6
+          actual = get_cached_result_for(17, 4)
+          _(actual).must_equal(expected)
+        end
       end
     end
 
@@ -63,30 +107,6 @@ describe Day11 do
         stones = Day11::PhysicsDefyingStones.new("2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2")
         expected = 22
         _(stones.count).must_equal(expected)
-      end
-    end
-
-    describe "#equality" do
-      it 'considers two different lists with the same input to be equal' do
-        input = "512072 1 20 24 28676032"
-        a = Day11::PhysicsDefyingStones.new(input)
-        b = Day11::PhysicsDefyingStones.new(input)
-        _(a).must_equal(b)
-      end
-
-      it 'considers two lists with the different input to be equal' do
-        input = "512072 1 20 24 28676032"
-        a = Day11::PhysicsDefyingStones.new(input)
-        b = Day11::PhysicsDefyingStones.new(input.reverse)
-        _(a).wont_equal(b)
-      end
-    end
-
-    describe "#to_s" do
-      it 'outputs the list in the same format as the input' do
-        input = "512072 1 20 24 28676032"
-        stones = Day11::PhysicsDefyingStones.new(input)
-        _(stones.to_s).must_equal(input)
       end
     end
   end
